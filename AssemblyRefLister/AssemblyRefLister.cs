d8f7bc2fbe06adbb09e93248e6f70a5088381c98
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-01-20 12:35:59 +0100

    +++ added initial project state

---
+++ AssemblyRefLister/AssemblyRefLister.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Pixelplastic
+{
+    public class AssemblyRefLister
+    {
+        readonly bool _ignoreGAC;
+        int _indention;
+        readonly IList<string> _touchedRefs;
+
+        public Action<string> PrintAction { get; private set; }
+
+        public AssemblyRefLister(bool ignoreGAC = true, Action<string> printAction = null)
+        {
+            _ignoreGAC = ignoreGAC;
+            _touchedRefs = new List<string>();
+            PrintAction = printAction ?? IndentedPrint;
+        }
+
+        void IndentedPrint(string value)
+        {
+            for(var i=0; i<_indention;i++) Console.Write(" ");
+
+            Console.WriteLine(value);
+        }
+
+        public void PrintAssemblyNameAndReferences(Assembly assembly)
+        {
+            if (_ignoreGAC && assembly.GlobalAssemblyCache) return;
+
+            _touchedRefs.Add(assembly.FullName);
+            PrintAction(assembly.FullName);
+
+            _indention++;
+            foreach (var referencedAssembly in assembly.GetReferencedAssemblies())
+            {
+                if (_touchedRefs.Contains(referencedAssembly.FullName)) continue;
+
+                PrintAssemblyNameAndReferences(referencedAssembly);
+            }
+            _indention--;
+        }
+
+        public void PrintAssemblyNameAndReferences(AssemblyName fullName)
+        {
+            try
+            {
+                PrintAssemblyNameAndReferences(Assembly.Load(fullName));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void PrintAssemblyNameAndReferences(string fullName)
+        {
+            if (!File.Exists(fullName)) PrintAssemblyNameAndReferences(new AssemblyName(fullName));
+
+            fullName = Path.GetFullPath(fullName);
+
+            AppDomain.CurrentDomain.AssemblyResolve +=
+                (s, a) =>
+                    {
+                        var folderPath = Path.GetDirectoryName(fullName);
+                        var assemblyName = new AssemblyName(a.Name);
+                        var assemblyPath = Path.Combine(folderPath, assemblyName.Name + ".dll");
+
+                        if (File.Exists(assemblyPath)) return Assembly.LoadFrom(assemblyPath);
+
+                        return null;
+                    };
+
+            try
+            {
+                PrintAssemblyNameAndReferences(Assembly.LoadFrom(fullName));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ AssemblyRefLister/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace Pixelplastic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                PrintUsage();
+                return;
+            }
+
+            var ignoreGAC = args.Length == 2 && args[1].ToLower() == "--no-gac";
+
+            new AssemblyRefLister(ignoreGAC).PrintAssemblyNameAndReferences(args[0]);
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("Usage: {0}.exe <NameOfAssembly>     # without .dll or .exe extension", Assembly.GetExecutingAssembly().GetName().Name);
+            Console.WriteLine("       --no-gac   # will skip assemblies from GAC");
+        }
+    }
+}
