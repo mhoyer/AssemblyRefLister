a3810e20804ee82b23193581734f2e3e9f65e443
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-01-20 13:09:21 +0100

    ~ fixed AssemblyInfo

--- AssemblyRefLister/Properties/AssemblyInfo.cs
+++ AssemblyRefLister/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AssemblyRefLister")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A console tool to quickly print out all references and dependencies of an assembly.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Pixelplastic")]
 [assembly: AssemblyProduct("AssemblyRefLister")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Marcel Hoyer 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
